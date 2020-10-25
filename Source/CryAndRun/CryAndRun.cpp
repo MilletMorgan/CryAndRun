7bc99f27edecf8fa706879dd64a2315aa80b93dd
Froznight <loic.papazian@outlook.fr>
2020-10-25 15:33:14 +0100

    Creation des class ZombieSt + Player

---
+++ Source/CryAndRun/CryAndRun.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CryAndRun.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CryAndRun, "CryAndRun" );
